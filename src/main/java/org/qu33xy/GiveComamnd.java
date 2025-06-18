818a53f916e560c66dba9811a45a7ad111f759a1
qu33xy <ahome@poczta.onet.pl>
2025-06-18 23:47:16 +0200

    .

--- src/main/java/org/qu33xy/GiveComamnd.java
+++ src/main/java/org/qu33xy/GiveComamnd.java
@@ -1,21 +1,23 @@
 package org.qu33xy;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class GiveComamnd implements CommandExecutor {
-    public String pluginName =  "&6&lFarmer&f&lPlugin ";
+    String pluginName = ChatColor.translateAlternateColorCodes('&', "&6&lFarmer&f&lPlugin");
+
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!sender.isOp()) {
-            sender.sendMessage(pluginName + "Nie masz uprawnien do tej komendy!");
+            sender.sendMessage(pluginName + " Nie masz uprawnien do tej komendy!");
             return true;
         }
         if (!(sender instanceof Player)) {
-            sender.sendMessage(pluginName + "Tylko gracz moze uzyc tej komendy!");
+            sender.sendMessage(pluginName + " Tylko gracz moze uzyc tej komendy!");
             return true;
         }
         Player player = (Player) sender;
@@ -23,18 +25,18 @@ public class GiveComamnd implements CommandExecutor {
         switch (label.toLowerCase()) {
             case "boyfarmer":
                 player.getInventory().addItem(Main.boyFarmer);
-                player.sendMessage(pluginName + "Dostales przedmiot: " + Main.boyFarmer.getItemMeta().getDisplayName());
+                player.sendMessage(pluginName + " Dostales przedmiot: " + Main.boyFarmer.getItemMeta().getDisplayName());
                 break;
             case "sandfarmer":
                 player.getInventory().addItem(Main.sandFarmer);
-                player.sendMessage(pluginName + "Dostales przedmiot: " + Main.sandFarmer.getItemMeta().getDisplayName());
+                player.sendMessage(pluginName + " Dostales przedmiot: " + Main.sandFarmer.getItemMeta().getDisplayName());
                 break;
             case "kopaczfosy":
                 player.getInventory().addItem(Main.kopaczFosy);
-                player.sendMessage(pluginName + "Dostales przedmiot: " + Main.kopaczFosy.getItemMeta().getDisplayName());
+                player.sendMessage(pluginName + " Dostales przedmiot: " + Main.kopaczFosy.getItemMeta().getDisplayName());
                 break;
             default:
-                player.sendMessage(pluginName + "Nieznana komenda!");
+                player.sendMessage(pluginName + " Nieznana komenda!");
         }
 
         return true;
